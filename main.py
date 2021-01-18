7431303d223874880a38481cda302bf508a55b63
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-01-18 23:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import datetime
 
+random_list = []
+
+
 try:
     os.remove('temp.jpg')
     os.remove('watermark.jpg')
@@ -13,14 +16,48 @@ try:
 except:
     None
 
-
+def random_check():
+    page_list = ['funny', 'dankmemes', 'memes', 'teenagers', 'Chodi', "DsyncTV", 'cursedcomments', 'holdup',
+                 'SaimanSays/', 'wholesomememes']
+    to_check = random.choice(page_list)
+    if len(random_list) < 6:
+        if len(random_list) == 0:
+            random_list.append(to_check)
+            print(random_list)
+            return to_check
+        else:
+            for pages in random_list:
+                if pages==to_check:
+                   # print('faied')
+                    return random_check()
+                else:
+                    random_list.append(to_check)
+                    print(random_list)
+                    return to_check
+    else:
+        t=0
+        for pages in random_list:
+            if pages==to_check:
+                t=0
+                break
+            else:
+                t=1
+                continue
+        if t ==1:
+            random_list.pop(0)
+            random_list.append(to_check)
+            print(random_list)
+            return to_check
+        else :
+            #print('faied')
+            return random_check()
 def meme_installer(target):
 
     reddit = praw.Reddit(client_id="OZsROIAyH5bAbA", client_secret='PhYFLRgpllL3ZPpdIQe3D5yhRWc', username="DK00167",
                          password="98766789",
                          user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36")
-    page_list = ['funny', 'dankmemes', 'memes', 'teenagers', 'Chodi', "DsyncTV", 'cursedcomments', 'holdup', 'SaimanSays/' , 'wholesomememes']
-    meme_page = random.choice(page_list)
+
+    meme_page = random_check()
     memes = reddit.subreddit(meme_page)
 
     if target == 1:
@@ -77,7 +114,7 @@ def insta_upload(title,page):
 
 
 while(True):
-    if(datetime.datetime.now().strftime("%X") == '10:30:00'):
+    if(datetime.datetime.now().strftime("%X") == '23:46:30'):
         try:
             if(datetime.datetime.now().strftime("%X") == 'Wednesday'):
                 title , page = meme_installer(target=2)
