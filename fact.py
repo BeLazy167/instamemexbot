52068a24c31d78893b05ae4603a23d730781660d
JayP09 <66618767+JayP09@users.noreply.github.com>
2021-02-02 12:57:18 +0530

    Updated fact.py

--- fact.py
+++ fact.py
@@ -9,7 +9,6 @@ import os
 
 ACCESS_KEY_UNSPLASH='j6-TI4_jxVWMUqgKc6K4zgHzFWChSZ6B-q6-cz3mk40'
 var1=0
-title=''
 
 def key_word():
     response = requests.get("https://uselessfacts.jsph.pl/random.json?language=en")
@@ -18,67 +17,60 @@ def key_word():
     r = Rake()
     r.extract_keywords_from_text(fact)
     key_word = r.get_ranked_phrases()
-    print(key_word)
-    title=key_word[0]
     unsplash_img(fact,key_word[0])
-    return "temp.jpg",title
+    return "fact_watermark.jpg",fact
 
 def unsplash_img(fact,query="fact"):
-    print(query)
     path='https://api.unsplash.com/search/photos?&client_id='+ACCESS_KEY_UNSPLASH+'&page=1&query='+query+'&orientation=landscape'
-    print(path)
     response=requests.get(path)
     img_data=response.json()
     img_url = img_data['results'][0]['urls']['regular']
-    print(img_url)
-    img_name ='temp.jpg'
+    img_name ='fact.jpg'
     url.urlretrieve(img_url, img_name)
-    editor(fact)
+    image_editor(fact)
 
-def editor(fact):
+
+def image_editor(fact):
     base = 1070
-    img = Image.open('temp.jpg')
+    img = Image.open('fact.jpg')#open image
     width, height = img.size
     if width >= 1070:
-        wpercent = (base/ float(img.size[0]))
+        wpercent = (base / float(img.size[0]))
         hsize = int((float(img.size[1]) * float(wpercent)))
         img = img.resize((base, hsize), Image.ANTIALIAS)
-        img.save('temp.jpg')
-    image_editor()
-    fact_watermark(fact)
 
-def image_editor():
-    im = Image.open('temp.jpg')  # open image
-    if im.size[0] >= im.size[1]:
-        whitespace = int((im.size[0] - im.size[1]) / 2) + 0
+    # below code is used to make image width/height ratio 1.0
+    if img.size[0] >= img.size[1]:
+        whitespace = int((img.size[0] - img.size[1]) / 2) + 0
         xbump = 0
     else:
-        xbump = int((im.size[1] - im.size[0]) / 2) + 0
+        xbump = int((img.size[1] - img.size[0]) / 2) + 0
         whitespace = 0
     var1 = whitespace
-    matted = ImageOps.expand(im, border=(xbump, whitespace),
+    matted = ImageOps.expand(img, border=(xbump, whitespace),
                              fill='white')
 
     # resize image to 1070x1070
     im = resizeimage.resize_cover(matted, [1070,1070])
-    im.save("temp.jpg")
+    im.save("fact.jpg")
     add_border_and_blur(5, "white")
+    fact_watermark(fact)
     return
 
 def add_border_and_blur(border, color='white'):
-    img = Image.open( 'temp.jpg' )
+    img = Image.open( 'fact.jpg' )
     if isinstance( border, int ) or isinstance( border, tuple ) :
         bimg = ImageOps.expand( img, border=border, fill=color )  # used to add border
     else :
         raise Exception( "Border is not an integer or tuple!" )
 
     im2 = bimg.filter(ImageFilter.GaussianBlur(radius=3))
-    im2.save("temp.jpg")
+    im2.save("fact.jpg")
     return
 
 def fact_watermark(fact) :
     # Create an Image Object from an Image
-    im = Image.open( 'temp.jpg' )
+    im = Image.open( 'fact.jpg' )
     img_width,img_height = im.size
 
     draw = ImageDraw.Draw( im )
@@ -103,29 +95,30 @@ def fact_watermark(fact) :
     w,h=con_font.getsize(new_fact)
     space=20
     y_text = (img_height) / 2 - var1 - ((int((h+space))*len(lines)/ 2)) + 30
-    draw_con.multiline_text((35, y_text),new_fact, font=con_font, fill=(0,0,0),spacing=space,stroke_width=3,stroke_fill=(255,255,255))
-    im.save("temp.jpg")
-    return "temp.jpg"
+    draw_con.multiline_text((35, y_text),new_fact, font=con_font, fill=(0,0,0),spacing=space,stroke_width=2,stroke_fill=(255,255,255))
+    im.save("fact_watermark.jpg")
+    return "fact_watermark.jpg"
 
 def delete():
     try:
-        os.remove('temp.jpg')
-        os.remove('temp.jpg.REMOVE_ME.jpg')
+        os.remove('fact_watermark.jpg.REMOVE_ME')
+        os.remove('fact.jpg')
+        os.remove('fact_watermark.jpg')
     except:
         print("nothing is deleted")
 
 def insta_upload_fact():
-    print("Input your instagram username and password to upload fact")
-    username = 'factbot__'# your username
-    password = 'jay2345' # your password
+    username = 'memebot_10101'# your username
+    password = 'memebot@10101' # your password
 
-    image,title = key_word()  # here you can put the image directory
-    final_caption = title +' \n #fact #facts #knowledge #didyouknow #factz #factsdaily #amazingfacts #factsoflife #love #science #dailyfacts #knowledgeispower #india #gk #generalknowledge #instafacts #instagram #interestingfacts #funfacts #follow #truefacts #life #factoftheday #coolfacts #sciencefacts #motivation #truth #didyouknowfacts #quotes #bhfyp'
+    image,fact = key_word()  # here you can put the image directory
+    final_caption = fact+' \n #fact #facts #knowledge #didyouknow #factz #factsdaily #amazingfacts #factsoflife #love #science #dailyfacts #knowledgeispower #india #gk #generalknowledge #instafacts #instagram #interestingfacts #funfacts #follow #truefacts #life #factoftheday #coolfacts #sciencefacts #motivation #truth #didyouknowfacts #quotes #bhfyp'
 
     bot = Bot()
     bot.login(username=username, password=password)
     bot.upload_photo(image, final_caption)
+    delete()
 
 insta_upload_fact()
-delete()
+
 
