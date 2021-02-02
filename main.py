84f176130a81b8b19353abdfc244d254971a92e1
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-02-02 13:20:55 +0530

    update main/py
    changed time

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import meme ,fact
 if __name__ == '__main__':
     while 1:
         x = datetime.datetime.now( )
-        if x.strftime("%X") == '13:17:30':
+        if x.strftime("%X") == '10:30:15':
             meme.full_run( )
             break
-        elif x.strftime("%X")== '22:30:30':
+        elif x.strftime("%X")== '22:30:15':
             fact.insta_upload_fact()
             break
         else:
