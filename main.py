8080baf042f2687316b50558c6763008c05a9bc3
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-02-08 08:46:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
         elif x.strftime("%X")== '22:31:00':
             fact.insta_upload_fact()
             break
+        elif x.strftime("%X")== '22:35:00' or x.strftime("%X")=='10:35:00':
+            break
         else:
             print(x.strftime("%X"))
             time.sleep(1)
